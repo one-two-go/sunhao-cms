b3c71945a636250708ed54fe403f05f4d25a5950
one-two-go <17600015348@163.com>
2019-11-12 11:32:34 +0800

    配置修改

---
+++ src/main/java/com/sunhao/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.sunhao.controller;
+
+/**
+ * 项目名称：sunhaocms
+ * 类 名 称：UserCOntroller
+ * 类 描 述：TODO
+ * 创建时间：2019/11/12 11:30 上午
+ * 创 建 人：sunhao
+ */
+public class UserController {
+
+}
