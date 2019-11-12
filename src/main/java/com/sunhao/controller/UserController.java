1de8ee9ee72e74278ba1b509c32a3fdf158ae6b2
one-two-go <17600015348@163.com>
2019-11-12 13:45:17 +0800

    初始化配置

--- src/main/java/com/sunhao/controller/UserController.java
+++ src/main/java/com/sunhao/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.sunhao.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 项目名称：sunhaocms
  * 类 名 称：UserCOntroller
@@ -7,6 +13,15 @@ package com.sunhao.controller;
  * 创建时间：2019/11/12 11:30 上午
  * 创 建 人：sunhao
  */
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public  String ConTest(HttpServletRequest request){
+
+        request.setAttribute("info","hello");
+        return "user/list";
+    }
 
 }
